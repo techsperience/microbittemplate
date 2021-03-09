c07cc91fc85bfd3221ed58159552f426a7091820
Jason Takahashi <75383299+techsperience@users.noreply.github.com>
2021-03-09 11:26:26 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
 basic.forever(function () {
 	
 })
